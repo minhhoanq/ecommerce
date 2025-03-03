877ceddd667108922d1b979f696d06f559a9d4c5
minhhoanq <mh.tranminhhoang@gmail.com>
2025-03-03 20:25:45 +0700

    setup kafka in payment service

--- payment_service/src/config/config.ts
+++ payment_service/src/config/config.ts
@@ -1,15 +1,18 @@
 import { token } from "brandi";
 import { DatabaseConfig } from "./database";
 import { GRPCServerConfig } from "./grpc_server";
+import { KafkaConfig } from "./kafka";
 
 export class Config {
     public databaseConfig = new DatabaseConfig();
     public grpcServerConfig = new GRPCServerConfig();
+    public kafkaConfig = new KafkaConfig();
 
     public static fromEnv(): Config {
         const config = new Config()
         config.databaseConfig = DatabaseConfig.fromEnv();
         config.grpcServerConfig = GRPCServerConfig.fromEnv();
+        config.kafkaConfig = KafkaConfig.fromEnv();
 
         return config;
     }
--- payment_service/src/config/index.ts
+++ payment_service/src/config/index.ts
@@ -2,6 +2,7 @@ import { Container } from "brandi";
 import { Config, CONFIG_TOKEN } from "./config";
 import { DATABASE_CONFIG_TOKEN } from "./database";
 import { GRPC_SERVER_CONFIG_TOKEN } from "./grpc_server";
+import { KAFKA_CONFIG_TOKEN } from "./kafka";
 
 export * from "./config"
 export * from "./database"
@@ -11,4 +12,5 @@ export function bindToContainer(container: Container): void {
     container.bind(CONFIG_TOKEN).toInstance(Config.fromEnv).inSingletonScope();
     container.bind(DATABASE_CONFIG_TOKEN).toInstance(() => Config.fromEnv().databaseConfig).inSingletonScope();
     container.bind(GRPC_SERVER_CONFIG_TOKEN).toInstance(() => Config.fromEnv().grpcServerConfig).inSingletonScope();
+    container.bind(KAFKA_CONFIG_TOKEN).toInstance(() => Config.fromEnv().kafkaConfig).inSingletonScope();
 }
